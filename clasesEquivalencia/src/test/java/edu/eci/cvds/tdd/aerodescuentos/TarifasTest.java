107d01531ee4cecd604c4e373a5ba96cd1d24a26
Deivid <deivid.medina@mail.escuelaing.edu.co>
2021-08-29 17:52:00 -0500

    Creando tarifasTest, falta implementar pruebas.

---
+++ clasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -0,0 +1,11 @@
+package edu.eci.cvds.tdd.aerodescuentos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class TarifasTest {
+	private CalculadorDescuentos calcularDescuento = new CalculadorDescuentos(); 
+}
