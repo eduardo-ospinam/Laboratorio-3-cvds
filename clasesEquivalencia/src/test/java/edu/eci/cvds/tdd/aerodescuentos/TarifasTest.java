e97ef8be5b7171cc55b504ff7cd2f0c1e9297e1f
Deivid <deivid.medina@mail.escuelaing.edu.co>
2021-08-31 18:40:24 -0500

    Finalizando Implementación de pruebas.

--- clasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ clasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -8,4 +8,40 @@ import junit.framework.Assert;
 
 public class TarifasTest {
 	private CalculadorDescuentos calcularDescuento = new CalculadorDescuentos(); 
+	
+	@Test
+	public void descuentoDiasAntelacionMasEdadMayor() throws Exception {
+		double CalculoTarifa = calcularDescuento.calculoTarifa(1000, 21, 70);
+		Assert.assertEquals(CalculoTarifa,770.0);			
+	}
+	
+	@Test
+	public void descuentoDiasAntelacionMasEdadMenor() throws Exception {
+		double CalculoTarifa = calcularDescuento.calculoTarifa(2000, 25, 15);
+		Assert.assertEquals(CalculoTarifa,1600.0);			
+	}
+	
+	@Test
+	public void descuentoEdadMenor() throws Exception {
+		double CalculoTarifa = calcularDescuento.calculoTarifa(3000, 15, 14);
+		Assert.assertEquals(CalculoTarifa,2850.0);			
+	}
+	
+	@Test
+	public void descuentoEdadMayor() throws Exception {
+		double CalculoTarifa = calcularDescuento.calculoTarifa(7000, 15, 80);
+		Assert.assertEquals(CalculoTarifa,6440.0);			
+	}
+	
+	@Test
+	public void descuentoDiasAntelacionSinEdad() throws Exception {
+		double CalculoTarifa = calcularDescuento.calculoTarifa(5000, 30, 20);
+		Assert.assertEquals(CalculoTarifa,4250.0);			
+	}
+	
+	@Test
+	public void descuentoNulo() throws Exception {
+		double CalculoTarifa = calcularDescuento.calculoTarifa(8000, 19, 25);
+		Assert.assertEquals(CalculoTarifa,8000.0);	 		
+	}
 }
